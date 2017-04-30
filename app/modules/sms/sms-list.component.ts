a66e31dbf87d6df66f9c8129903f76c78e056819
Thiago Garcia <thiagoufg@gmail.com>
2017-04-30 20:12:54 -0300

    listing SMSs

--- app/modules/sms/sms-list.component.ts
+++ app/modules/sms/sms-list.component.ts
@@ -1,18 +1,29 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import * as TNSInbox from 'nativescript-sms-inbox';
 
 @Component({
   selector: 'home',
   templateUrl: 'modules/sms/sms-list.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  //changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SmsListComponent implements OnInit {
-  
+
   items: Array<Object> = [];
 
   ngOnInit(): void {
+    this.getInboxMessages();
     this.items.push({ name: "Apples" });
     this.items.push({ name: "Bananas" });
     this.items.push({ name: "Oranges" });
   }
 
+  public getInboxMessages() { //fromNumber = "0712345678"
+    TNSInbox.getInboxes({ max: 20 }).then((res) => {
+
+      let resposta: Array<Object> = res.data;
+      this.items = resposta.map((res) => { return { name: res['message'] }; });
+      console.dump(this.items);
+    }, (err) => { console.log(err); });
+
+  }
 }
